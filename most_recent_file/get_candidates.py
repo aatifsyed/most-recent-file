f294e9b5cc53e5e033aaf88fb4090738d1919d2f
Aatif Syed <aatifsyedyp@gmail.com>
2021-07-01 22:05:44 +0100

    new core

--- most_recent_file/get_candidates.py
+++ most_recent_file/get_candidates.py
@@ -1,36 +1,68 @@
-from typing import Iterable
-from pathlib import Path, WindowsPath
-from itertools import chain
+from itertools import tee
+from pathlib import Path
+from typing import Iterable, Iterator
+
+import git
+from git import InvalidGitRepositoryError
 
 
 def is_hidden(path: Path) -> bool:
     return path.name.startswith(".")
 
 
-def with_descendants(
-    path: Path, /, descend_hidden_folders: bool, include_hidden_files: bool
+def has_hidden_parent(path: Path, /, relative_to: Path) -> bool:
+    return any(
+        map(
+            is_hidden,
+            path.relative_to(relative_to).parents,
+        ),
+    )
+
+
+def remove_gitignored(paths: Iterator[Path], /, root: Path) -> Iterator[Path]:
+    try:
+        repo = git.Repo(path=root, search_parent_directories=True)
+    except InvalidGitRepositoryError:
+        return paths
+
+    dot_git = Path(repo.common_dir)
+    paths = filter(lambda path: not path.is_relative_to(dot_git), paths)
+
+    p0, p1 = tee(paths)
+
+    ignored = set(map(Path, repo.ignored(*p0)))
+
+    return filter(lambda path: path not in ignored, p1)
+
+
+def get_candidates(
+    root: Path,
+    /,
+    recurse: bool,
+    include_hidden_files: bool,
+    include_gitignored: bool,
+    include_folders: bool,
+    descend_hidden_directories: bool,
 ) -> Iterable[Path]:
-    if not include_hidden_files and is_hidden(path):
-        yield from ()
+    root = root.resolve()
+
+    if recurse:
+        paths = iter(root.rglob("*"))
     else:
-        yield path
+        paths = iter([root])
 
-    for child in path.glob("*"):
-        if is_hidden(child):
-            if child.is_dir() and descend_hidden_folders:
-                yield from with_descendants(child)
-            else:
-                raise NotImplementedError
-        else:
-            yield from with_descendants(
-                child,
-                descend_hidden_folders=descend_hidden_folders,
-                include_hidden_files=include_hidden_files,
-            )
+    if not include_hidden_files:
+        paths = filter(lambda path: not is_hidden(path) and path.is_file(), paths)
 
+    if not descend_hidden_directories:
+        paths = filter(
+            lambda path: not has_hidden_parent(path, relative_to=root), paths
+        )
 
-def get_candidates(path: Path, /, recurse: bool) -> Iterable[Path]:
-    if path.is_file() and not recurse:
-        return [path]
-    else:
-        return []
+    if not include_gitignored:
+        paths = remove_gitignored(paths, root=root)
+
+    if not include_folders:
+        paths = filter(lambda path: not path.is_dir(), paths)
+
+    return paths
---
+++ tests/test_get_candidates.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import git
+from more_itertools import ilen
+
+import most_recent_file.get_candidates as subject
+
+
+def test_is_hidden():
+    assert subject.is_hidden(Path(".hidden"))
+    assert subject.is_hidden(Path("nested/.hidden"))
+    assert not subject.is_hidden(Path("visible"))
+    assert not subject.is_hidden(Path(".nested/visible"))
+
+
+def test_has_hidden_parent():
+    assert subject.has_hidden_parent(
+        Path("before/.hidden/after/visible"), relative_to=Path("before")
+    )
+    assert not subject.has_hidden_parent(
+        Path("before/.hidden/after/visible"), relative_to=Path("before/.hidden/after")
+    )
+    assert not subject.has_hidden_parent(Path(".hidden"), relative_to=Path())
+
+
+def test_remove_gitignored(tmp_path: Path):
+    git.Repo.init(path=tmp_path)
+    tmp_path.joinpath("included").touch()
+    included = subject.remove_gitignored(iter(tmp_path.rglob("*")), root=tmp_path)
+    assert ilen(included) == 1  # Ignored .git contents
+
+    tmp_path.joinpath("excluded").touch()
+    tmp_path.joinpath(".gitignore").write_text("/excluded")
+
+    included = subject.remove_gitignored(iter(tmp_path.rglob("*")), root=tmp_path)
+    assert ilen(included) == 2  # Including .gitignore
